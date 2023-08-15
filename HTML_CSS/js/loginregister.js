ec2b8a8045bffdd8c473297ece426008f0117a94
rhoolan <rhoolan@gmail.com>
2023-08-15 18:04:47 +0900

    End of day update

---
+++ HTML_CSS/js/loginregister.js
@@ -0,0 +1,9 @@
+function registerSlideUp(){
+    document.getElementById('register').style = 'display: block';
+    document.getElementById('login').style = 'display: none'
+}
+
+function loginSlideUp(){
+    document.getElementById('login').style = "display: block";
+    document.getElementById('register').style = "display: none";
+}
