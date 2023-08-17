0c46fc62ee769da6a2652f3021e4a0b4463996ed
rhoolan <rhoolan@gmail.com>
2023-08-17 17:10:00 +0900

    Add UserRegisterController

--- blog/src/main/java/blog/ex/controller/UserRegisterController.java
+++ blog/src/main/java/blog/ex/controller/UserRegisterController.java
@@ -2,6 +2,10 @@ package blog.ex.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import blog.ex.service.UserService;
 
@@ -13,5 +17,14 @@ public class UserRegisterController {
 	@Autowired
 	private UserService userService;
 	
-	@GetMapping("/register")
+	@GetMapping("/loginregister")
+	public String getLoginRegisterPage() {
+		return "loginregister.html";
+	}
+	
+	@PostMapping("/register/registerprocess")
+	public String register(@RequestParam String user_name, @RequestParam String user_email, @RequestParam String user_password) {
+		userService.createAccount(user_name, user_email, user_password);
+		return "redirect:/loginregister";
+	}
 }
