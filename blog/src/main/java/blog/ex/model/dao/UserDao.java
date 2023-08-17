9de0e25779aca7ccafe8b3b99ba7749baba5d617
rhoolan <rhoolan@gmail.com>
2023-08-17 16:50:11 +0900

    Add UserDao

---
+++ blog/src/main/java/blog/ex/model/dao/UserDao.java
@@ -0,0 +1,10 @@
+package blog.ex.model.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import blog.ex.model.entity.UserEntity;
+
+public interface UserDao extends JpaRepository<UserEntity, Long> {
+	UserEntity save(UserEntity userEntity);
+	UserEntity findByEmail(String user_email);
+}
